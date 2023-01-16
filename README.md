edb9258a07415592aefc555e93bc035b6ad895db
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-16 11:26:37 +0600

    moon-tech-rtk-query

---
+++ README.md
@@ -0,0 +1,1 @@
+## Moon Tech RTK Query
---
+++ src/app/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "../features/cart/cartSlice";
+import filterSlice from "../features/filter/filterSlice";
+import logger from "redux-logger";
+
+const store = configureStore({
+  devTools: true,
+  reducer: {
+    cart: cartSlice,
+    filter: filterSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
