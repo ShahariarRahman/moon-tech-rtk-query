c55433a8dcb199a43ff97c72d92432f63f528c0c
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-18 07:06:07 +0600

    connect reducer at store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../features/cart/cartSlice";
 import filterSlice from "../features/filter/filterSlice";
 import logger from "redux-logger";
+import { productApi } from "../features/api/apiSlice";
 
 const store = configureStore({
   devTools: true,
   reducer: {
+    [productApi.reducerPath]: productApi.reducer,
     cart: cartSlice,
     filter: filterSlice,
   },
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const productApi = createApi({
+export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "http://localhost:5000",
   }),
   endpoints: (builder) => ({
     getProduct: builder.query({
-      query: () => "product",
+      query: () => "/product",
     }),
   }),
 });
